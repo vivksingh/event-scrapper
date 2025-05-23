95953fc705df7b9d3bc978ad2268d78233180162
vek <vivksing655@gmail.com>
2025-05-23 12:13:14 +0530

    updated api url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ const EventProvider = ({ children }) => {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    fetch("https://event-scrapper-alxd.onrender.com/events")
+    fetch("https://event-scrapper-alxd.onrender.com/api/events")
       .then((res) => res.json())
       .then((data) => setEvents(data))
       .catch((err) => console.error("Failed to fetch events", err));
